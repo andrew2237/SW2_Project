759642f1258c5502f80d8e3b995c8d8bd4396fc9
Varino Alfried <66516413+varino-alfried@users.noreply.github.com>
2022-05-11 23:29:19 +0200

    CleanCode Version1 For AssignmentClass
    CleanCode Version1 For AssignmentClass
    Added:
    ---------
    1) IAssignmentDB.java (interface)
    2)AssignmentDB.java (class)
    3) IInputManager.java (interface)
    4)AssignmentInput.java (class)
    *Added the new class call at the connection.java (main)

---
+++ IInputManager.java
@@ -0,0 +1,19 @@
+package mamgment;
+
+
+public interface IInputManager {
+
+    public int InputID();
+
+    public String InputName();
+
+    public String InputDate();
+
+    public String InputType();
+   
+    public int InputDeleteOptions();
+    
+    public int InputUpdateOptions();
+}
+
+ 
